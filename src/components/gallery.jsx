fe3698b9139e07340669dd971fed740bf5eb97f3
matheus hopper <mhjc@cin.ufpe.br>
2022-10-21 11:47:24 -0300

    update videos

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -32,6 +32,9 @@ export const Gallery = (props) => {
           <iframe width="390" height="833" src="https://www.youtube.com/embed/5WuWPYapu9s" title="Formatura da primeira turma da ResidêncIA em Robótica do CIn - UFPE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <iframe width="585" height="400" src="https://www.youtube.com/embed/QEt43pkAzVo" title="Dia das crianças CIn/Softex" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <iframe width="585" height="400" src="https://www.youtube.com/embed/37ylskxBBUA" title="Dia das crianças CIn/Softex" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="585" height="400" src="https://www.youtube.com/embed/oLhBFcmoqW8" title="Apresentação do Projeto Pelo Professor Alexandre à Softex - Parte 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="585" height="400" src="https://www.youtube.com/embed/j0dN981M17M" title="Apresentação do Projeto Pelo Professor Alexandre à Softex - Parte 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="585" height="400" src="https://www.youtube.com/embed/M0pAhhV3TuY" title="Reportagem CINTEC" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
     </div>
