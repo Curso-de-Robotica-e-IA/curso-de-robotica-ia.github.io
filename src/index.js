88867754b3cbbd04eb6c5c6fc6848b84feacacba
Riei-Joaquim <rjmr@cin.ufpe.br>
2022-12-13 09:34:04 -0300

    fix routers

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import * as serviceWorker from "./serviceWorker";
 import { AllTeam } from "./pages/allTeam";
 import { AllTestimonials } from "./pages/allTestimonials";
 import { AllGallery } from "./pages/allGallery";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Project2022 } from "./pages/projects/project2022";
 import { Project } from "./components/projects/2022/project";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={< App />} />
       <Route path="/all-team" element={< AllTeam />} />
@@ -20,7 +21,7 @@ ReactDOM.render(
       <Route path="/blogpost" element={< Project />} />
       <Route path="/project-0" element={< Project2022 />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
